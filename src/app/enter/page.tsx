db33bfc628fd897d042fb4d2465531053d494880
Sergey Shulga <shul.sergio@gmail.com>
2026-01-10 15:44:42 +0200

    test2

---
+++ src/app/enter/page.tsx
@@ -0,0 +1,16 @@
+import css from "./enter.module.css";
+
+export default function Home() {
+  return (
+    <div className={css.page}>
+      <main className={css.main}>
+        <div className={css.intro}>
+          <p className={css.descriptionTop}>Welcome to</p>
+          <h1 className={css.title}>SCORY</h1>
+          <p className={css.descriptionBottom}>write your story of glory</p>
+        </div>
+        <div className={css.comingSoon}>coming soon</div>
+      </main>
+    </div>
+  );
+}
