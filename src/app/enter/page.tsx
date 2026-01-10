1c7cffd59c7996b0131a45378988f4a8a8504dd1
Sergey Shulga <shul.sergio@gmail.com>
2026-01-11 00:14:11 +0200

    Update page.tsx

--- src/app/enter/page.tsx
+++ src/app/enter/page.tsx
@@ -9,6 +9,7 @@ export default function Enter() {
           <h1 className={css.title}>SCORY</h1>
           <p className={css.descriptionBottom}>write your story of glory</p>
           <p>PREVIEW</p>
+          <p>NEW NEW NEW </p>
         </div>
         <div className={css.comingSoon}>coming soon</div>
       </main>
