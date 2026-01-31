d2725f8685b6d49239955cd325b5e3049a95c841
Sergey Shulga <shul.sergio@gmail.com>
2026-01-31 15:24:13 +0200

    add matches data on front

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -31,43 +31,76 @@ interface Match {
 export default async function Matches() {
   const matches = await fetchAllMatches();
   console.log("!!!! Matches:", matches);
+
+  const data = matches?.map((match: Match) => ({
+    data: new Date(match.kickoffTime).toLocaleDateString("ru-RU"),
+  }));
+  console.log("data", data);
+  const newDates = Array.from(
+    new Set(
+      matches.map((match: Match) =>
+        new Date(match.kickoffTime).toLocaleDateString("ru-RU"),
+      ),
+    ),
+  ) as string[];
+  newDates.sort((a, b) => {
+    return (
+      new Date(a.split(".").reverse().join("-")).getTime() -
+      new Date(b.split(".").reverse().join("-")).getTime()
+    );
+  });
+  console.log("!!!! newDates", newDates);
   return (
     <div>
       <h1>Next matches</h1>
+      {/* ..... первій мар .....  */}
+      {newDates.map((date: string) => (
+        <div key={date}>
+          <h2>{date}</h2>
+          {/* ..... первій мар .....  */}
+          <table>
+            <tbody>
+              {/* ..... второй фільтр і мар .....  */}
+              {matches
+                .filter(
+                  (match: Match) =>
+                    new Date(match.kickoffTime).toLocaleDateString("ru-RU") ===
+                    date,
+                )
 
-      <table>
-        <thead>
-          <tr>
-            <th>Teams</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {matches &&
-            matches.map((match: Match) => (
-              <tr key={match._id}>
-                <td>
-                  <Link href={`/teams/${match.homeTeam?._id}`}>
-                    {match.homeTeam?.name || "-"}
-                  </Link>
-                  <br />
-                  <Link href={`/teams/${match.awayTeam?._id}`}>
-                    {match.awayTeam?.name || "-"}
-                  </Link>
-                </td>
+                .map((match: Match) => (
+                  <tr key={match._id}>
+                    <td>
+                      {new Date(match.kickoffTime).toLocaleTimeString("ru-RU", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </td>
 
-                <td>
-                  {new Date(match.kickoffTime).toLocaleDateString("ru-RU")}
-                  <br />
-                  {new Date(match.kickoffTime).toLocaleTimeString("ru-RU", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+                    <td>
+                      <div>
+                        <Link href={`/teams/${match.homeTeam?._id}`}>
+                          {match.homeTeam?.name || "-"}
+                        </Link>
+                      </div>
+                      <div>
+                        <Link href={`/teams/${match.awayTeam?._id}`}>
+                          {match.awayTeam?.name || "-"}
+                        </Link>
+                      </div>
+                    </td>
+
+                    <td>
+                      {match.stadium}
+                      <br />
+                      <span>Group {match.group}</span>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
     </div>
   );
 }
