ff649bc7a114541932ef6f79c03b7bdaedd2a9ba
Sergey Shulga <shul.sergio@gmail.com>
2026-02-11 23:57:35 +0200

    add new components

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,23 @@
+import UserStatus from "@/components/UserStatus/UserStatus";
+import css from "./profile.module.css";
+
+/**
+ * Страница Профиля юзера после входа
+ * Должно быть 3 блока - статус юзера, матчи, лиги
+ *
+ * @export
+ * @return {* }
+ *
+ */
 export default function Profile() {
   return (
-    <>
-      <h1>ТІ ЗАШЕЛ!!!</h1>
-    </>
+    <main className={css.mainContainer}>
+      <h2 className={css.title}>Профиль</h2>
+      <UserStatus />
+      {/* <h2 className={css.title}>Matches</h2>
+      <UserStatus />
+      <h2 className={css.title}>Leagues</h2>
+      <UserStatus /> */}
+    </main>
   );
 }
