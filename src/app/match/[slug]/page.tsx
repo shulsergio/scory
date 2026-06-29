e1bdb0c03fca0c7a9f6fd65c54ef2dabe1e455ec
Sergey Shulga <shul.sergio@gmail.com>
2026-06-29 14:39:08 +0300

    adit css preview

--- src/app/match/[slug]/page.tsx
+++ src/app/match/[slug]/page.tsx
@@ -34,7 +34,6 @@ export default async function MatchDetailPage({ params }: MatchPageProps) {
 
   const isFinished = match.status === "finished";
 
-  // Вытягиваем данные превью, чтобы код не пестрил длинными цепочками ?.
   const preview = match.preview;
   const infoBox = preview?.infoBox;
   const h2h = preview?.headToHead;
@@ -74,128 +73,137 @@ export default async function MatchDetailPage({ params }: MatchPageProps) {
         <div className={css.teamBlock}>
           <span className={css.teamName}>{match.awayTeam?.name || "wait"}</span>
         </div>
-      </div>
-      {/* ДЕТАЛИ (STADIUM & REFEREE) */}
-      {infoBox && (
-        <section className={css.detailsSection}>
-          <h3 className={css.blockTitle}>Match Details</h3>
-          <div className={css.detailsGrid}>
-            {infoBox.Stadium && (
-              <div className={css.detailsCard}>
-                <span className={css.label}>Stadium</span>
-                <span className={css.value}>
-                  {infoBox.Stadium.name}{" "}
-                  {infoBox.Stadium.capacity
-                    ? ` (${infoBox.Stadium.capacity.toLocaleString()})`
-                    : ""}
-                </span>
-                <p className={css.subValue}>
-                  {infoBox.Stadium.city}, <br />
-                  <span className={css.value}>{infoBox.Stadium.country}</span>
-                </p>
-              </div>
-            )}
-            {infoBox.Referee?.name && (
-              <div className={css.detailsCard}>
-                <span className={css.label}>Referee</span>
-                <span className={css.value}>{infoBox.Referee.name}</span>
-                <span className={css.subValue}>{infoBox.Referee.country}</span>
-              </div>
-            )}
-            {preview?.weather && (
-              <div className={css.detailsCard}>
-                <span className={css.label}>Weather</span>
-                <div className={css.weatherContainer}>
-                  {/* температура */}
-                  <span className={css.weatherTemp}>
-                    {preview.weather.temperature > 0
-                      ? `+${preview.weather.temperature}`
-                      : preview.weather.temperature}
-                    °C,{" "}
-                    {preview.weather.windSpeed && (
-                      <span className={css.subValue}>
-                        {preview.weather.windSpeed} m/s
-                      </span>
-                    )}
+      </div>{" "}
+      <div className={css.dataBoxContainer}>
+        {/* ДЕТАЛИ (STADIUM & REFEREE) */}
+        {infoBox && (
+          <section className={css.detailsSection}>
+            <h3 className={css.blockTitle}>Match Details</h3>
+            <div className={css.tableWrapper}>
+              {infoBox.Stadium && (
+                <div className={css.detailsCard}>
+                  <span className={css.label}>Stadium</span>
+                  <span className={css.value}>
+                    {infoBox.Stadium.name}{" "}
+                    {infoBox.Stadium.capacity
+                      ? ` (${infoBox.Stadium.capacity.toLocaleString()})`
+                      : ""}
                   </span>
+                  <p className={css.subValue}>
+                    {infoBox.Stadium.city}, <br />
+                    <span className={css.value}>{infoBox.Stadium.country}</span>
+                  </p>
                 </div>
+              )}
+              {/* ----- */}
+              <div className={css.horizontalBox}>
+                {infoBox.Referee?.name && (
+                  <div className={css.detailsCard}>
+                    <span className={css.label}>Referee</span>
+                    <span className={css.value}>{infoBox.Referee.name}</span>
+                    <span className={css.subValue}>
+                      {infoBox.Referee.country}
+                    </span>
+                  </div>
+                )}
+                {preview?.weather && (
+                  <div className={css.detailsCard}>
+                    <span className={css.label}>Weather</span>
+                    <div className={css.weatherContainer}>
+                      {/* температура */}
+                      <span className={css.weatherTemp}>
+                        {preview.weather.temperature > 0
+                          ? `+${preview.weather.temperature}`
+                          : preview.weather.temperature}
+                        °C,{" "}
+                        {preview.weather.windSpeed && (
+                          <span className={css.subValue}>
+                            {preview.weather.windSpeed} m/s
+                          </span>
+                        )}
+                      </span>
+                    </div>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-        </section>
-      )}
+              {/* ----- */}
+            </div>
+          </section>
+        )}
 
-      {/*  (HEAD TO HEAD) */}
+        {/*  (HEAD TO HEAD) */}
 
-      {h2h && h2h.matches && h2h.matches.length > 0 && (
-        <section className={css.h2hSection}>
-          <h3 className={css.blockTitle}>Head to Head History</h3>
+        {h2h && h2h.matches && h2h.matches.length > 0 && (
+          <section className={css.h2hSection}>
+            <h3 className={css.blockTitle}>Head to Head History</h3>
+            <div className={css.tableWrapper}>
+              {/* Сводка побед/ничьих (Summary) */}
+              {h2h.summary && h2h.summary.length === 3 && (
+                <div className={css.summaryBar}>
+                  <div className={css.summaryItem}>
+                    <span className={css.summaryCount}>{h2h.summary[0]}</span>
+                    <span className={css.summaryLabel}>
+                      Wins {match.homeTeam?.name}
+                    </span>
+                  </div>
+                  <div className={css.summaryItem}>
+                    <span className={css.summaryCount}>{h2h.summary[1]}</span>
+                    <span className={css.summaryLabel}>Draws</span>
+                  </div>
+                  <div className={css.summaryItem}>
+                    <span className={css.summaryCount}>{h2h.summary[2]}</span>
+                    <span className={css.summaryLabel}>
+                      Wins {match.awayTeam?.name}
+                    </span>
+                  </div>
+                </div>
+              )}
 
-          {/* Сводка побед/ничьих (Summary) */}
-          {h2h.summary && h2h.summary.length === 3 && (
-            <div className={css.summaryBar}>
-              <div className={css.summaryItem}>
-                <span className={css.summaryCount}>{h2h.summary[0]}</span>
-                <span className={css.summaryLabel}>
-                  Wins {match.homeTeam?.name}
-                </span>
-              </div>
-              <div className={css.summaryItem}>
-                <span className={css.summaryCount}>{h2h.summary[1]}</span>
-                <span className={css.summaryLabel}>Draws</span>
-              </div>
-              <div className={css.summaryItem}>
-                <span className={css.summaryCount}>{h2h.summary[2]}</span>
-                <span className={css.summaryLabel}>
-                  Wins {match.awayTeam?.name}
-                </span>
+              {/* Список прошлых матчей */}
+              <div className={css.h2hTableWrapper}>
+                <table className={css.h2hTable}>
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th className={css.textRight}>Home</th>
+                      <th className={css.textCenter}>Score</th>
+                      <th className={css.textLeft}>Away</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {h2h.matches.map((prevMatch, index) => {
+                      const formattedPrevDate = new Date(
+                        prevMatch.date,
+                      ).toLocaleDateString("ru-RU", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                      });
+
+                      return (
+                        <tr key={prevMatch._id || index}>
+                          <td className={css.h2hDate}>{formattedPrevDate}</td>
+                          <td className={`${css.h2hTeam} ${css.textRight}`}>
+                            {prevMatch.home}
+                          </td>
+                          <td className={`${css.h2hScore} ${css.textCenter}`}>
+                            <span className={css.h2hScoreBadge}>
+                              {prevMatch.score}
+                            </span>
+                          </td>
+                          <td className={`${css.h2hTeam} ${css.textLeft}`}>
+                            {prevMatch.away}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
               </div>
             </div>
-          )}
-
-          {/* Список прошлых матчей */}
-          <div className={css.h2hTableWrapper}>
-            <table className={css.h2hTable}>
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th className={css.textRight}>Home</th>
-                  <th className={css.textCenter}>Score</th>
-                  <th className={css.textLeft}>Away</th>
-                </tr>
-              </thead>
-              <tbody>
-                {h2h.matches.map((prevMatch, index) => {
-                  const formattedPrevDate = new Date(
-                    prevMatch.date,
-                  ).toLocaleDateString("ru-RU", {
-                    day: "2-digit",
-                    month: "2-digit",
-                    year: "numeric",
-                  });
-
-                  return (
-                    <tr key={prevMatch._id || index}>
-                      <td className={css.h2hDate}>{formattedPrevDate}</td>
-                      <td className={`${css.h2hTeam} ${css.textRight}`}>
-                        {prevMatch.home}
-                      </td>
-                      <td className={`${css.h2hScore} ${css.textCenter}`}>
-                        <span className={css.h2hScoreBadge}>
-                          {prevMatch.score}
-                        </span>
-                      </td>
-                      <td className={`${css.h2hTeam} ${css.textLeft}`}>
-                        {prevMatch.away}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </section>
-      )}
+          </section>
+        )}
+      </div>
     </main>
   );
 }
